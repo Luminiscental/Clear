8f65c0c1f876acf38ae18001b4207f136204b51b
Luminiscental <luminiscental01@gmail.com>
2019-02-21 19:28:49 +0000

    Refactor file I/O in clrc

--- ClearC/clrc.py
+++ ClearC/clrc.py
@@ -17,23 +17,25 @@ def main():
 
     with open(source_file_name, 'r') as source_file:
         source = source_file.read()
-        try:
-            byte_code = parse_source(source)
-            if debug:
-                disassemble(byte_code)
-        except CompileException as e:
-            print('Could not compile:')
-            print(e)
-        else:
-            with open(dest_file_name, 'wb') as dest_file:
-                raw_bytes = bytearray()
-                for code in byte_code:
-                    byte = bytes([code])[0]
-                    print(byte)
-                    raw_bytes.append(byte)
-                print(raw_bytes)
-                print(dest_file_name)
-                dest_file.write(raw_bytes)
+
+    try:
+        byte_code = parse_source(source)
+        if debug:
+            disassemble(byte_code)
+    except CompileException as e:
+        print('Could not compile:')
+        print(e)
+    else:
+        raw_bytes = bytearray()
+        for code in byte_code:
+            byte = bytes([code])[0]
+            print(byte)
+            raw_bytes.append(byte)
+        print(raw_bytes)
+        print(dest_file_name)
+
+        with open(dest_file_name, 'wb') as dest_file:
+            dest_file.write(raw_bytes)
 
 if __name__ == '__main__':
 
