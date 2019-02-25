7183d6fae7492a890fbf2ce30b44ba2d944a7e1c
Luminiscental <luminiscental01@gmail.com>
2019-02-25 18:45:10 +0000

    Implement pratt parser

--- ClearC/clr/compile.py
+++ ClearC/clr/compile.py
@@ -4,6 +4,12 @@ from enum import Enum
 from clr.tokens import tokenize, TokenType
 from clr.errors import ClrCompileError
 
+def emit_error(message):
+
+    def emission():
+        raise ClrCompileError(message)
+    return emission
+
 class OpCode(Enum):
 
     STORE_CONST = 0
@@ -24,6 +30,20 @@ class OpCode(Enum):
     def __str__(self):
         return 'OP_' + self.name
 
+class Precedence(Enum):
+
+    NONE = 0
+    ASSIGNMENT = 1
+    OR = 2
+    AND = 3
+    EQUALITY = 4
+    COMPARISON = 5
+    TERM = 6
+    FACTOR = 7
+    UNARY = 8
+    CALL = 9
+    PRIMARY = 10
+
 class Constants:
 
     def __init__(self):
@@ -41,17 +61,17 @@ class Constants:
     def flush(self):
         code_list = []
         for value in self.values:
-            if isinstance(value, float):
-                code_list.append(OpCode.STORE_CONST)
-                code_list.append(OpCode.NUMBER)
-                code_list.append(value)
-            elif isinstance(value, str):
-                code_list.append(OpCode.STORE_CONST)
-                code_list.append(OpCode.STRING)
-                code_list.append(value)
-            else:
-                raise ClrCompileError('Unknown constant value type: {}'
-                        .format(value))
+            code_list.append(OpCode.STORE_CONST)
+            value_type = type(value)
+
+            op_type = {
+                float: lambda: OpCode.NUMBER,
+                str: lambda: OpCode.STRING
+            }.get(value_type, emit_error('Unknown constant value type: {}'
+                        .format(value_type)))()
+
+            code_list.append(op_type)
+            code_list.append(value)
         return code_list
 
 class Program:
@@ -108,6 +128,13 @@ def assemble(code_list):
             raw_bytes.append(byte)
     return raw_bytes
 
+class ParseRule:
+
+    def __init__(self, infix=None, prefix=None, precedence=None):
+        self.infix = infix if infix else emit_error('Expected expression')
+        self.prefix = prefix if prefix else emit_error('Expected expression')
+        self.precedence = precedence if precedence else Precedence.NONE
+
 class Cursor:
 
     def __init__(self, tokens):
@@ -119,19 +146,93 @@ class Cursor:
     def get_current(self):
         return self.tokens[self.index]
 
+    def get_last(self):
+        return self.tokens[self.index - 1]
+
     def advance(self):
         self.index += 1
 
+    def get_rule(self, token):
+        return {
+            TokenType.LEFT_PAREN : ParseRule(
+                prefix=self.finish_grouping,
+                precedence=Precedence.CALL
+            ),
+            TokenType.MINUS : ParseRule(
+                prefix=self.finish_unary,
+                infix=self.finish_binary,
+                precedence=Precedence.TERM
+            ),
+            TokenType.PLUS : ParseRule(
+                infix=self.finish_binary,
+                precedence=Precedence.TERM
+            ),
+            TokenType.SLASH : ParseRule(
+                infix=self.finish_binary,
+                precedence=Precedence.FACTOR
+            ),
+            TokenType.STAR : ParseRule(
+                infix=self.finish_binary,
+                precedence=Precedence.FACTOR
+            ),
+            TokenType.NUMBER : ParseRule(
+                prefix=self.consume_number
+            ),
+            TokenType.AND : ParseRule(
+                precedence=Precedence.AND
+            ),
+            TokenType.OR : ParseRule(
+                precedence=Precedence.OR
+            )
+        }.get(token.token_type, ParseRule()) 
+
+    def consume_number(self):
+        token = self.get_last()
+        if token.token_type != TokenType.NUMBER:
+            emit_error('Expected number token!')()
+        const_index = self.constants.add(float(token.lexeme))
+        self.program.load_constant(const_index)
+
+    def consume_precedence(self, precedence):
+        self.advance()
+        self.get_rule(self.get_last()).prefix()
+        while precedence.value <= self.get_rule(self.get_current()).precedence.value:
+            self.advance()
+            self.get_rule(self.get_last()).infix()
+
+    def finish_grouping(self):
+        self.consume_expression()
+        self.consume(TokenType.RIGHT_PAREN, 'Expect ) after expression')
+
+    def finish_unary(self):
+        op_token = self.get_last()
+        self.consume_precedence(Precedence.UNARY)
+        {
+            TokenType.MINUS : self.program.op_negate
+        }.get(op_token.token_type, emit_error('Expected unary operator'))()
+
+    def finish_binary(self):
+        op_token = self.get_last()
+        rule = self.get_rule(op_token)
+        self.consume_precedence(rule.precedence)
+        {
+            TokenType.PLUS : self.program.op_add,
+            TokenType.MINUS : self.program.op_subtract,
+            TokenType.STAR : self.program.op_multiply,
+            TokenType.SLASH : self.program.op_divide
+        }.get(op_token.token_type, emit_error('Expected binary operator'))()
+
     def consume_expression(self):
-        pass
+        self.consume_precedence(Precedence.ASSIGNMENT)
 
     def consume(self, expected_type, message):
         if self.get_current().token_type == expected_type:
             self.advance()
         else:
-            raise ClrCompileError(message)
+            emit_error(message)()
 
     def flush(self):
+        self.program.op_return()
         return self.constants.flush() + self.program.flush()
 
 def parse_source(source):
@@ -140,29 +241,9 @@ def parse_source(source):
 
     print(' '.join(map(lambda token: token.lexeme, tokens)))
 
-    """
-
     cursor = Cursor(tokens)
     cursor.consume_expression()
     cursor.consume(TokenType.EOF, "Expect end of expression.")
+
     return assemble(cursor.flush())
-
-    """
-
-    constants = Constants()
-    program = Program()
-
-    const_a = constants.add(13.2)
-    const_b = constants.add(24.7)
-    const_c = constants.add("Hello World!")
-
-    program.load_constant(const_a)
-    program.load_constant(const_b)
-    program.op_divide()
-    program.op_print()
-    program.load_constant(const_c)
-    program.op_print()
-    program.op_return()
-
-    return assemble(constants.flush() + program.flush())
 
