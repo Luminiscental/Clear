c7b26b574789d3a438fa412b9bb16d9f2f0f657d
Luminiscental <luminiscental01@gmail.com>
2019-03-16 22:27:53 +0000

    Add docstrings to clr.ast.decl

--- ClearC/clr/ast/decl.py
+++ ClearC/clr/ast/decl.py
@@ -1,3 +1,14 @@
+"""
+This module provides classes / functions to form declaration nodes within the AST from parsing tokens.
+
+Functions:
+    - parse_decl
+
+Classes:
+    - DeclNode
+    - FuncDecl
+    - ValDecl
+"""
 from clr.tokens import TokenType
 from clr.errors import parse_error
 from clr.ast.stmt import BlockStmt, StmtNode, parse_stmt
@@ -6,6 +17,19 @@ from clr.ast.index import IndexAnnotation
 
 
 def parse_decl(parser):
+    """
+    This function parses a declaration node for the AST from the parser, emitting an error if the
+    tokens don't form a valid declaration.
+
+    The declaration follows the grammar rule
+    declaration : ValDecl | FuncDecl | statement ;
+
+    Parameters:
+        - parser : the Parser instance to read tokens from.
+
+    Returns:
+        The Declaration node that was parsed.
+    """
     if parser.check_one(VAL_TOKENS):
         return ValDecl(parser)
     if parser.check(TokenType.FUNC):
@@ -14,12 +38,41 @@ def parse_decl(parser):
 
 
 class DeclNode(StmtNode):
+    """
+    This class stores the index annotation for an AST node of a declaration, inheriting from
+    StmtNode and to be inherited in any declaration node classes.
+
+    Superclasses:
+        - StmtNode
+
+    Fields:
+        - index_annotation : the index annotation of the declared value; by default unresolved.
+    """
+
     def __init__(self, parser):
         super().__init__(parser)
         self.index_annotation = IndexAnnotation()
 
 
 class FuncDecl(DeclNode):
+    """
+    This class represents an AST node for a function declaration, initialized from a parser.
+
+    It follows the grammar rule
+    FuncDecl : 'func' identifier '(' ( identifier identifier ( ',' identifier identifier )* )? ')' BlockStmt
+
+    Superclasses:
+        - DeclNode
+
+    Fields:
+        - name : the identifier token naming the declared function.
+        - params : a list of (identifier, identifier) tuples for the parameters of the declared function.
+        - block : the BlockStmt of code defining the function.
+
+    Methods:
+        - accept
+    """
+
     def __init__(self, parser):
         super().__init__(parser)
         parser.consume(
@@ -54,10 +107,34 @@ class FuncDecl(DeclNode):
         self.token = self.name
 
     def accept(self, decl_visitor):
+        """
+        This method accepts a declaration visitor to the node.
+
+        Parameters:
+            - decl_visitor : the visitor to accept.
+        """
         decl_visitor.visit_func_decl(self)
 
 
 class ValDecl(DeclNode):
+    """
+    This class reprents an AST node for a value declaration, initialized from a parser.
+
+    It follows the grammar rule
+    ValDecl : ( 'val' | 'var' ) identifier '=' expression ';' ;
+
+    Superclasses:
+        - DeclNode
+
+    Fields:
+        - mutable : boolean for whether the declared value is mutable.
+        - name : the identifier token for the declared value.
+        - value : the expression initializing the declared value.
+
+    Methods:
+        - accept
+    """
+
     def __init__(self, parser):
         super().__init__(parser)
         parser.consume_one(
@@ -79,6 +156,12 @@ class ValDecl(DeclNode):
         self.token = self.name
 
     def accept(self, decl_visitor):
+        """
+        This method accepts a declaration visitor to the node.
+
+        Parameters:
+            - decl_visitor : the visitor to accept.
+        """
         decl_visitor.visit_val_decl(self)
 
 
