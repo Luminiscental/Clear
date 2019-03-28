3026215910d26356f3231db7fb31b55ce5b2bf25
Luminiscental <luminiscental01@gmail.com>
2019-03-28 19:17:09 +0000

    Separate out parser class

---
+++ ClearC/clr/ast/parser.py
@@ -0,0 +1,49 @@
+from clr.tokens import token_info
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.index = 0
+        self.tokens = tokens
+
+    def get_current(self):
+        return self.tokens[self.index]
+
+    def get_prev(self):
+        return self.tokens[self.index - 1]
+
+    def current_info(self):
+        return token_info(self.get_current())
+
+    def prev_info(self):
+        return token_info(self.get_prev())
+
+    def advance(self):
+        self.index += 1
+
+    def check(self, token_type):
+        return self.get_current().token_type == token_type
+
+    def check_one(self, possibilities):
+        return self.get_current().token_type in possibilities
+
+    def match(self, expected_type):
+        if not self.check(expected_type):
+            return False
+        self.advance()
+        return True
+
+    def match_one(self, possibilities):
+        if not self.check_one(possibilities):
+            return False
+        self.advance()
+        return True
+
+    def consume(self, expected_type, err):
+        if not self.match(expected_type):
+            err()
+
+    def consume_one(self, possibilities, err):
+        if self.match_one(possibilities):
+            return
+        err()
--- ClearC/clr/ast/tree.py
+++ ClearC/clr/ast/tree.py
@@ -20,6 +20,7 @@ from clr.ast.type_annotations import (
 )
 from clr.ast.return_annotations import ReturnAnnotation, ReturnAnnotationType
 from clr.ast.index_annotations import IndexAnnotationType, IndexAnnotation
+from clr.ast.parser import Parser
 
 
 Builtin = namedtuple("Builtin", ("signatures", "opcode", "return_type"))
@@ -47,54 +48,6 @@ BUILTINS = {
         return_type=BOOL_TYPE,
     ),
 }
-
-
-class Parser:
-    def __init__(self, tokens):
-        self.index = 0
-        self.tokens = tokens
-
-    def get_current(self):
-        return self.tokens[self.index]
-
-    def get_prev(self):
-        return self.tokens[self.index - 1]
-
-    def current_info(self):
-        return token_info(self.get_current())
-
-    def prev_info(self):
-        return token_info(self.get_prev())
-
-    def advance(self):
-        self.index += 1
-
-    def check(self, token_type):
-        return self.get_current().token_type == token_type
-
-    def check_one(self, possibilities):
-        return self.get_current().token_type in possibilities
-
-    def match(self, expected_type):
-        if not self.check(expected_type):
-            return False
-        self.advance()
-        return True
-
-    def match_one(self, possibilities):
-        if not self.check_one(possibilities):
-            return False
-        self.advance()
-        return True
-
-    def consume(self, expected_type, err):
-        if not self.match(expected_type):
-            err()
-
-    def consume_one(self, possibilities, err):
-        if self.match_one(possibilities):
-            return
-        err()
 
 
 class SimpleType:
